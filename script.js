124c0d86329311fdc6b5a3911257a6ff6a22d757
Sandra Krejcir <krejcir.sandra@gmail.com>
2022-09-02 15:28:50 +0200

    added the showing of info, no css tho

--- script.js
+++ script.js
@@ -21,9 +21,24 @@ function calcBMI() {
   let user = new User(inputName, inputWeight, inputHeight);
   userArray.push(user);
   console.log(userArray);
+  showUsers();
   resetInput();
 }
 
+function showUsers() {
+  userArray.forEach((user) => {
+    const template = document.querySelector("#infoTemplate").content;
+    const copy = template.cloneNode(true);
+    copy.querySelector("#titleName").textContent = user.userName;
+    copy.querySelector("#weightSec").textContent =
+      "Your weight is: " + user.userWeight + "kg";
+    copy.querySelector("#heightSec").textContent =
+      "Your height is: " + user.userHeight + "m";
+    copy.querySelector("#BMISec").textContent = "Your BMI is: " + user.userBMI;
+    const parent = document.querySelector("#infoSec");
+    parent.appendChild(copy);
+  });
+}
 function resetInput() {
   document.querySelector("#name").value = "";
   document.querySelector("#height").value = "";
